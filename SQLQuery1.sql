d9ef778f5a8419e10f22daef45592e4b7054a5ce
Rajajr <rajjr011@gmail.com>
2022-11-03 06:53:03 +0700

    UC-2 Ability To Create AddressBook Table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,4 @@
 --UC1-Ability To Create a Address Book Service DataBase
 create database Address_Book_Services;
+--UC2-Ability to create a Address Book Service Table
+create table AddressBook(First_Name varchar(100),Last_Name varchar(100),States varchar(100),City varchar(100),Zip varchar(100),Home_address varchar(200),Phone_No bigint,Email varchar(30));
